26f6dfe588a96d7f11ff0237bfcb2fd1acb87cc4
kunalpawar83 <kunalpawar8319@gmail.com>
2024-11-11 09:50:44 +0530

    complete basic gemini

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+require("dotenv").config();
+
+const app = express();
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post("/api/gemini", async (req, res) => {
+  try {
+    const prompt = req.body.question;
+    const result = await model.generateContent(prompt);
+    console.log(result.response.text());
+    res.status(200).json({
+      status: "success",
+      response: result.response.text(),
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+module.exports = app;
